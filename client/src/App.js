1db775b0d8c18621c11013f8ecc76cf23025ec16
erc83 <espinoza.eric@gmail.com>
2021-09-06 15:33:10 -0300

    render Home auth in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import useStyles from './styles';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 
 const App = () => {
@@ -15,8 +16,10 @@ const App = () => {
     <Router>
       <Container maxWidth="lg">
         <Navbar />
-        <Home />
-
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/auth" exact component={Auth}/>
+        </Switch>
       </Container>
     </Router>  
   );
