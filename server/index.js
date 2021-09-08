73b54c5f0511cbd1457b12700a92a84db81e8afc
erc83 <espinoza.eric@gmail.com>
2021-09-08 00:21:36 -0300

    sign in and sign up backend

--- server/index.js
+++ server/index.js
@@ -6,14 +6,16 @@ import dotenv from "dotenv"
 dotenv.config();
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js'
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes)
 
 const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT|| 5000;
