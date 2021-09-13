3da576307d966e3360fd1fe3e77b485a105b6d70
erc83 <espinoza.eric@gmail.com>
2021-09-13 11:17:29 -0300

    Updates

---
+++ precache-manifest.1610b52fd56eca575969a8fb45d8b8ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20161995d0b8fd338b7256747b10cd26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc90423df48acc25d05d",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "505f083daa05cf99bc72",
+    "url": "/static/js/2.1432063e.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.1432063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc90423df48acc25d05d",
+    "url": "/static/js/main.1511838a.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
