43d98daf6bae05e70edea65cc7c11fab372b49f5
erc83 <espinoza.eric@gmail.com>
2021-09-13 10:26:08 -0300

    ultimo commit para despliegue

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from "dotenv"
@@ -21,7 +21,7 @@ const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT|| 5500;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
